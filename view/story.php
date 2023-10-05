7d9ed60c4964d236421e0a315bd641a3662bbbfc
reynaudfrancois <freynaut@laposte.net>
2023-10-05 22:11:00 +0200

    Update story.php

--- view/story.php
+++ view/story.php
@@ -2,10 +2,33 @@
 
 <main id="story">
 
+	<h2>LE RECIT</h2>
+
 	<div class="stage">
 		<h2>Etape 1 : Fumel - Villefranche-du-Périgord</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<img src="">
+		<div>
+			<h3>Lorem ipsum</h3>
+			<img src="">
+		</div>
+	</div>
+
+	<div class="stage">
+		<h2>Etape 1 : Fumel - Villefranche-du-Périgord</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<div>
+			<h3>Lorem ipsum</h3>
+			<img src="">
+		</div>
+	</div>
+
+	<div class="stage">
+		<h2>Etape 1 : Fumel - Villefranche-du-Périgord</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<div>
+			<h3>Lorem ipsum</h3>
+			<img src="">
+		</div>
 	</div>
 
 	<div class="stage">
@@ -17,49 +40,55 @@
 	<div class="stage">
 		<h2>Etape 1 : Fumel - Villefranche-du-Périgord</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<img src="">
+		<div>
+			<h3>Lorem ipsum</h3>
+			<img src="">
+		</div>
 	</div>
 
 	<div class="stage">
 		<h2>Etape 1 : Fumel - Villefranche-du-Périgord</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<img src="">
+		<div>
+			<h3>Lorem ipsum</h3>
+			<img src="">
+		</div>
 	</div>
 
 	<div class="stage">
 		<h2>Etape 1 : Fumel - Villefranche-du-Périgord</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<img src="">
+		<div>
+			<h3>Lorem ipsum</h3>
+			<img src="">
+		</div>
 	</div>
 
 	<div class="stage">
 		<h2>Etape 1 : Fumel - Villefranche-du-Périgord</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<img src="">
+		<div>
+			<h3>Lorem ipsum</h3>
+			<img src="">
+		</div>
 	</div>
 
 	<div class="stage">
 		<h2>Etape 1 : Fumel - Villefranche-du-Périgord</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<img src="">
+		<div>
+			<h3>Lorem ipsum</h3>
+			<img src="">
+		</div>
 	</div>
 
 	<div class="stage">
 		<h2>Etape 1 : Fumel - Villefranche-du-Périgord</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<img src="">
-	</div>
-
-	<div class="stage">
-		<h2>Etape 1 : Fumel - Villefranche-du-Périgord</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<img src="">
-	</div>
-
-	<div class="stage">
-		<h2>Etape 1 : Fumel - Villefranche-du-Périgord</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<img src="">
+		<div>
+			<h3>Lorem ipsum</h3>
+			<img src="">
+		</div>
 	</div>
 	
 </main>
