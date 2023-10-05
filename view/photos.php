b0b1ab5ffb976ce544e0fd1cbedb9213f4c445cb
reynaudfrancois <freynaut@laposte.net>
2023-10-05 22:54:31 +0200

    Creation of the file photos.php

---
+++ view/photos.php
@@ -0,0 +1,25 @@
+<?php require "header.php"; ?> 
+
+	<main id="photos">
+		
+		<h2>LES PHOTOS</h2>
+
+		<div class="imagesTrek">
+
+			<div>
+				<button id="before"> &lt; </button>
+				<div id="sliderContainer">
+					<img name="slide" src="../public/images/imagesOfSlider/0.JPG" id="imagesOfSlider" alt="Perigord" />
+				</div>
+				<button id="next"> &gt; </button>
+				<h4 id="titleImage"></h4>
+				<h4 id="counterImage"></h4>			
+			</div>
+
+			<script src="../public/javascript/slider.js"></script>		
+
+		</div>
+
+	</main>
+
+<?php require "footer.php"; ?> 
