4f4c7d5457409ae2f0824ad749fb82d1b45d4d37
reynaudfrancois <freynaut@laposte.net>
2023-10-05 19:11:54 +0200

    Update header.php

---
+++ view/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" type="text/css" href="/projetStudi/public/css/normalize-css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="/projetStudi/public/fontawesome/css/all.css" />
+		<link rel="stylesheet" href="/projetStudi/public/css/style.css" />
+		<title>La traversée du Périgord</title>
+	</head>
+
+	<body>
+		
+		<header>
+
+			<nav>
+				<a href="/projetStudi/index.php">PRESENTATION</a>
+				<a href="/projetStudi/view/story.php">RECIT</a>
+				<a href="/projetStudi/view/photos.php">PHOTOS</a>
+				<a href="/projetStudi/view/comments.php">COMMENTAIRES</a>
+			</nav>
+
+			<div id="title">
+				<h1>La traversée des Alpes Du Sud</h1>
+				<h2>Du 28 septembre au 9 octobre 2023</h2>
+				<h3>par François REYNAUD</h3>
+			</div>
+			
+		</header>
