a9e276999086c5372b5e1237d53e85259e80eb84
reynaudfrancois <freynaut@laposte.net>
2023-10-05 22:28:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,25 +5,25 @@
 	<div id="presentation">
 		<h2>PRESENTATION</h2>
 		<p>Au début de l'automne 2023, entre le 28 septembre et le 9 octobre, j'ai décider d'effectuer une randonnée dand le département de la Dordogne, qui correspond au Périgord. Cette randonnée s'est effectuée en 10 étapes, à travers le Périgord Noir, qui correspond à l'arrondissement de Sarlat-la-Canéda, et le Périgord Vert, qui correspond à l'arrondissement de Nontron.</p>
-		<img src="">
+		<div><img src="public/images/image1.JPG"></div>
 	</div>
 
 	<div id="project">
 		<h3>Le projet</h3>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<img src="">
+		<div><img src="public/images/image1.JPG"></div>
 	</div>
 
 	<div id="path">
 		<h3>L'itinéraire</h3>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<img src="">
+		<div><img src="public/images/image1.JPG"></div>
 	</div>
-	
+
 	<div id="where">
 		<h3>Le Périgord</h3>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<img src="">
+		<div><img src="public/images/image1.JPG"></div>
 	</div>
 	
 </main>
