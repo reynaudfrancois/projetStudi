ba77e6ce304af89dd259521650964f4854e5e323
reynaudfrancois <freynaut@laposte.net>
2023-10-10 21:22:09 +0200

    Update comments.php

--- view/comments.php
+++ view/comments.php
@@ -1,6 +1,30 @@
 <?php require "header.php"; ?>
 
-<?php
+<main id="comments">
+
+	<div>		
+		<h2 class="generalTitle">LES COMMENTAIRES</h2>				
+
+		<button class="button" id="addComment">AJOUTER UN COMMENTAIRE<br /><i class="fas fa-angle-down" id="faAngle"></i></button>
+
+		<form id="commentForm" action="comments.php?action=addComment" method="post">
+		
+			<div class="field">
+				<label for="pseudo">Votre pseudo</label>
+				<input type="text" name="pseudo" id="pseudo" value="" />
+			</div>
+
+			<div class="field">
+				<label for="commentary">Votre commentaire</label>
+				<textarea name="commentary" id="commentary"></textarea>
+			</div>
+			
+			<div><input type="submit" name="send" class="button" value="Envoyer" /></div>
+
+		</form>
+	</div>
+
+	<?php
 
 
 
@@ -49,29 +73,12 @@ function viewComments ($errorMessage) {
 
 ?>
 
-<main id="comments">
+<?php 
 
-	<div>		
-		<h2 class="generalTitle">LES COMMENTAIRES</h2>				
+$pseudo = htmlspecialchars($_POST["pseudo"]);
+$commentary = htmlspecialchars($_POST["commentary"]);
 
-		<button class="button" id="addComment">AJOUTER UN COMMENTAIRE<br /><i class="fas fa-angle-down" id="faAngle"></i></button>
-
-		<form id="commentForm" action="" method="post">
-		
-			<div class="field">
-				<label for="pseudo">Votre pseudo</label>
-				<input type="text" name="pseudo" id="pseudo" value="" />
-			</div>
-
-			<div class="field">
-				<label for="commentary">Votre commentaire</label>
-				<textarea name="commentary" id="commentary"></textarea>
-			</div>
-			
-			<div><input type="submit" name="send" class="button" value="Envoyer" /></div>
-
-		</form>
-	</div>
+?>
 
 	<img src="../public/images/image1.JPG" alt="image1" />
 </main>	
