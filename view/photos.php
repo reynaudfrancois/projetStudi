19081688cba8be8ec3dc09d09c89073c5421525c
reynaudfrancois <freynaut@laposte.net>
2023-10-11 21:29:23 +0200

    Creation of file style.css

--- view/photos.php
+++ view/photos.php
@@ -5,18 +5,23 @@
 		<h2 class=pageTitle>LES PHOTOS</h2>
 
 		<div class="imagesTrek">
-
-			<div>
-				<button id="before"> &lt; </button>
-				<div id="sliderContainer">
-					<img name="slide" src="../public/images/imagesOfSlider/0.JPG" id="imagesOfSlider" alt="Perigord" />
-				</div>
-				<button id="next"> &gt; </button>
-				<h4 id="titleImage"></h4>
-				<h4 id="counterImage"></h4>			
+			
+			<div id="sliderContainer">
+				<img name="slide" src="../public/images/imagesOfSlider/0.JPG" id="imagesOfSlider" alt="Perigord" />
 			</div>
 
-			<script src="../public/javascript/slider.js"></script>		
+			<div id="buttonSlider">
+				<div>
+					<button id="before"><strong>&lt;</strong> Image précédente</button>
+					<button id="next">Image suivante <strong>&gt;</strong></button>
+				</div>
+				<h4 id="titleImage"></h4>
+				<h4 id="counterImage"></h4>
+			</div>
+
+		</div>
+
+			<script src="../public/javascript/script.js"></script>		
 
 		</div>
 
